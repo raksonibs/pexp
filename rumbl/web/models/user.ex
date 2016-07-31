029938b020b66f31748ae86c4d1448cac6af26fd
Oskar Niburski <oskarniburski@gmail.com>
2016-07-31 13:23:52 -0400

    New user

--- rumbl/web/models/user.ex
+++ rumbl/web/models/user.ex
@@ -1,3 +1,24 @@
 defmodule Rumbl.User do
-  defstruct [:id, :name, :username, :password]
+  # defstruct [:id, :name, :username, :password]
+
+  use Rumbl.Web, :model
+
+  schema "users" do 
+    field :name, :string
+    field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps
+  end
+
+  def changeset(model, params \\ %{}) do 
+    # name and username are requred, with no optinal fields (cast checks for this)
+    # we pipe cast, which returns an Ecto.Changeset, into a validate_length propery
+    model
+    |> cast(params, ~w(name username))
+    |> validate_length(:username, min: 1, max: 20)
+  end
+
+  # virtual field is an intermediate field before hasing password into hash. not peresisted.
 end
