99350b75c304ab28b300dd4e2060c73f7e356bc2
Oskar Niburski <oskarniburski@gmail.com>
2016-08-09 20:06:31 -0400

    Planet update

--- physics/lib/physics/rocketry.ex
+++ physics/lib/physics/rocketry.ex
@@ -1,7 +1,7 @@
 defmodule Physics.Rocketry do
   import Calcs
   import Physics.Laws
-  import Planets
+  import Planet
   import Converter
   
   # def force_calc(planet) when is_map(planet) do 
@@ -10,21 +10,6 @@ defmodule Physics.Rocketry do
   #     |> Converter.to_velocity
   #     |> Converter.round_to
   # end
-  
-  def escape_velocity(:moon) do 
-    moon |> escape_velocity
-  end
-
-  def escape_velocity(:earth) do 
-    earth |> escape_velocity
-  end
-
-  def escape_velocity(planet) when is_map(planet) do 
-    planet 
-      |> calculate_escape
-      |> Converter.convert_to_km
-      |> Converter.round_to
-  end
 
   # defp calculate_ortbital_distance_equation(mass, radius) do 
   #   newtons_gravitational_constant * mass / radius |> square_root
@@ -36,7 +21,7 @@ defmodule Physics.Rocketry do
   # end
 
   def orbital_radius_calc(time) do
-    newtons_gravitational_constant * earth.mass * (time |> squared) / 4 * (pi |> squared)
+    newtons_gravitational_constant * Planet.planets[:earth].mass * (time |> squared) / 4 * (pi |> squared)
       |> third_root
       |> convert_to_km
   end
@@ -46,7 +31,7 @@ defmodule Physics.Rocketry do
   end
 
   def orbital_term(height) do 
-    4 * (pi |> squared) * (orbital_radius(height) |> cubed) / (newtons_gravitational_constant * earth.mass)
+    4 * (pi |> squared) * (orbital_radius(height) |> cubed) / (newtons_gravitational_constant * Planet.planets[:earth].mass)
       |> square_root
       |> seconds_to_hours
   end
@@ -57,11 +42,11 @@ defmodule Physics.Rocketry do
   end
 
   def orbital_speed(height) do 
-    newtons_gravitational_constant * Planets.earth.mass / orbital_radius(height)
+    newtons_gravitational_constant * Planet.planets[:earth].mass / orbital_radius(height)
       |> square_root
   end
 
   defp orbital_radius(height) do
-      Planets.earth.radius + (height |> to_meters)
+      Planet.planets[:earth].radius + (height |> to_meters)
   end
 end
--- physics/lib/solar.ex
+++ physics/lib/solar.ex
@@ -11,4 +11,44 @@ defmodule Solar do
   def deadliest(flares) do 
     Enum.map(flares, &(power(&1))) |> Enum.max
   end
+
+  def total_flare_power(list) do 
+    total_flare_power(list, 0)
+  end
+
+  def total_flare_power([], total), do: total
+
+  # def total_flare_power([ head | tail], total) do 
+  #   new_total = power(head) + total 
+  #   total_flare_power(tail, new_total)
+  # end
+
+  # def total_flare_power([%{classification: :M, scale: s} | tail], total) do 
+  #   new_total = s * 10 * 0.92 + total
+  #   total_flare_power(tail, new_total)
+  # end
+
+  # def total_flare_power(flares) do 
+  #   Enum.reduce flares, 0, fn(flare, total) -> power(flare) + total end
+  # end
+
+  # def total_flare_power(flares) do 
+  #   (for flare <- flares, do: power(flare)) |> Enum.sum
+  # end
+
+  def flare_list(flares) do 
+    # Enum.map(flares, fn(flare) -> [power: power(flare), is_deadly: power(flare) > 1000])
+    #   |> List.flatten
+
+    Enum.map(flares, fn(flare) ->
+      p = power(flare)
+      {:flare, :power, p, is_deadly: p > 1000}
+    end)
+
+    # for flare <- flares, flare.classification == :X, do: %{power: power(flare), is_deadly: power(flare) > 1000}
+    # for flare <- flares,
+    # power <- [power(flare)],
+    # is_deadly <- [power > 1000],
+    # do: %{power: power, is_deadly: is_deadly}
+  end
 end
