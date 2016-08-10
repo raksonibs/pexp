28f4662dd73713b2f4eaa087f5f263bc8ffa6ec5
Oskar Niburski <oskarniburski@gmail.com>
2016-08-09 22:26:42 -0400

    Agent example

--- physics/lib/solar_flare_recorder.ex
+++ physics/lib/solar_flare_recorder.ex
@@ -3,19 +3,30 @@ defmodule SolarFlareRecorder do
 
     def start_link do
       # creates pcors in vm binding current module to process idand an empty list )our accumlator
-      GenServer.start_link __MODULE__, []
+      # GenServer.start_link __MODULE__, []
+      Agent.start_link fn -> [] end
     end
 
     def record_flare(pid, {:flare, classification: c, scale: s} = flare) do 
       #  take data in public api and tell genserver to call process that's identified by passed in pid, gen server look through all localling running process and then send module assocated with that pid the data we specify, in this case {:record, flare}
-      GenServer.call(pid, {:record, flare})
+      # GenServer.call(pid, {:record, flare})
+      Agent.get_and_update pid, fn(flares) -> 
+        new_state = List.insert_at flares, -1, flare 
+        {:ok, new_state}
+      end
     end
 
-    def handle_call({:record, flare}, _sender, flares) do 
-      # when finds the process and module bound to it, excute handle_call function.
-      # has the data passed to it, the sender, and the state accumlated so far
-      # only want to drop the new flare data into the list
-      flares = List.insert_at flares, -1, flare
-      {:reply, :ok, flares}
+    def get_flares(pid) do 
+      Agent.get pid, fn(flares) ->
+        flares 
+      end
     end
+
+    # def handle_call({:record, flare}, _sender, flares) do 
+    #   # when finds the process and module bound to it, excute handle_call function.
+    #   # has the data passed to it, the sender, and the state accumlated so far
+    #   # only want to drop the new flare data into the list
+    #   # flares = List.insert_at flares, -1, flare
+    #   # {:reply, :ok, flares}
+    # end
 end
